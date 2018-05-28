03abd0bfcf43042b623b1ab50d8732547f0863bb
Muhammad Fari <fari.msenju@gmail.com>
2018-05-29 05:27:59 +0700

    Tugas Phase 1

--- js/core.js
+++ js/core.js
@@ -1,8 +1,8 @@
+const port = process.env.PORT || 3500;
 app.controller('main_control',function($scope,$http){
 			loaded_mekaniks();
-			function loaded_mekaniks(){
-
-					$http.get("http://localhost:3500/load").success(function(data){
+			function loaded_mekaniks(){					
+					$http.get(`http://mekanik-ku.herokuapp.com:${port}/load`).success(function(data){
 							$scope.loaded_mekaniks=data;
 						})
 
